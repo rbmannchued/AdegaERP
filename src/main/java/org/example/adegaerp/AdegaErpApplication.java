a40fc86ad3223356105ceacab7039409709925a6
Rafael Bormann Chuede <rafaelchuede@gmail.com>
2025-02-12 21:23:20 -0300

    first commit

---
+++ src/main/java/org/example/adegaerp/AdegaErpApplication.java
@@ -0,0 +1,13 @@
+package org.example.adegaerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdegaErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdegaErpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/adegaerp/AdegaErpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.adegaerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdegaErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
